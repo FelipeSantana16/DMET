82616c53d3df4a3c6754d8003642e96528558064
Felipe Santana <felipepipa16@hotmail.com>
2020-08-14 11:43:18 -0300

    Projeto setado para viewcode

---
+++ DMET/DMET/Controller/CatalogViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CatalogViewController.swift
+//  DMET
+//
+//  Created by Felipe Santana on 14/08/20.
+//  Copyright © 2020 Felipe Santana. All rights reserved.
+//
+
+import UIKit
+
+class CatalogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
